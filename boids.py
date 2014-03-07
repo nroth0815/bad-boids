04042d4fff3223988859431fe28cc56160d02c80
nroth <n.roth@ucl.ac.uk>
2014-03-07 16:07:52 +0000

    Changed boids from list to Class.

--- boids.py
+++ boids.py
@@ -15,14 +15,37 @@ limit_2=10000
 middle_const=0.01
 speed_const=0.125
 
-boids_x=[random.uniform(-450,50.0) for x in range(range_val)]
-boids_y=[random.uniform(300.0,600.0) for x in range(range_val)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(range_val)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(range_val)]
-boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+#boids_x=[random.uniform(-450,50.0) for x in range(range_val)]
+#boids_y=[random.uniform(300.0,600.0) for x in range(range_val)]
+#boid_x_velocities=[random.uniform(0,10.0) for x in range(range_val)]
+#boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(range_val)]
+#boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+
+class Boids(object):
+  def __init__(self, input_list=None, flag=0 ):
+    if flag==0:
+      self.random_init()
+    if flag==1:
+      self.from_file(input_list)
+    
+  def random_init(self):
+    self.x=[random.uniform(-450,50.0) for x in range(range_val)]
+    self.y=[random.uniform(300.0,600.0) for x in range(range_val)]
+    self.x_velocity=[random.uniform(0,10.0) for x in range(range_val)]
+    self.y_velocity=[random.uniform(-20.0,20.0) for x in range(range_val)]
+    
+  def from_file(self, input_list):
+    self.x=input_list[0]
+    self.y=input_list[1]
+    self.x_velocity=input_list[2]
+    self.y_velocity=input_list[3]
+
+boids=Boids()
+#print boids.y
+#print type(boids)
 
 def update_boids(boids):
-	xs,ys,xvs,yvs=boids
+	xs,ys,xvs,yvs=boids.x, boids.y, boids.x_velocity, boids.y_velocity
 	# Fly towards the middle
 	for i in range(len(xs)):
 		for j in range(len(xs)):
@@ -50,11 +73,11 @@ def update_boids(boids):
 
 figure=plt.figure()
 axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(boids[0],boids[1])
+scatter=axes.scatter(boids.x,boids.y)
 
 def animate(frame):
    update_boids(boids)
-   scatter.set_offsets(zip(boids[0],boids[1]))
+   scatter.set_offsets(zip(boids.x,boids.y))
 
 
 anim = animation.FuncAnimation(figure, animate,
--- test_boids.py
+++ test_boids.py
@@ -1,13 +1,14 @@
-from boids import update_boids
+from boids import update_boids, Boids
 from nose.tools import assert_almost_equal
 import os
 import yaml
 
 def test_bad_boids_regression():
     regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
-    boid_data=regression_data["before"]
+    boid_data_in=regression_data["before"]
+    boid_data=Boids(input_list=boid_data_in, flag=1)
     update_boids(boid_data)
-    for after,before in zip(regression_data["after"],boid_data):
+    for after,before in zip(regression_data["after"],[boid_data.x,boid_data.y,boid_data.x_velocity, boid_data.y_velocity]):
         for after_value,before_value in zip(after,before): 
             assert_almost_equal(after_value,before_value,delta=0.01)
 	
